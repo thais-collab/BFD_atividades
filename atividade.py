af6e5256b8f0cab8069b4e74488adb2ff73a7a43
thais-collab <thais.adryene2056@gmail.com>
2025-08-16 14:19:19 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,61 @@
+print("Ola Mundo!")
+# Saida esperada: Ola Mundo!
+
+variavel_nome = input ("Qual o seu nome?")
+print("Ola", variavel_nome)
+input ("tudo bem com voce?")
+print("Que bom!")
+# Tudo ok!
+
+print ("Eu sei fazer calculos matematicos, ex. 80+50...")
+input ("voce quer ver a soma desses valores?")
+Num1 = 80
+Num2 = 50
+Soma = Num1 + Num2
+print("O resultado e:", Soma)
+# Tudo ok!
+
+variavel = input("Informe um numero qualquer:")
+print("O numero informado foi:", variavel)
+# tudo ok
+
+nome = ('Thais') 
+Sobrenome = ("Adryene")
+Variavel3 = 18
+print ("nome:", nome)
+print("sobrenome:", Sobrenome)
+print(" idade:", Variavel3)
+# tudo ok
+
+filme = ("Jonh_Wick")
+genero = ("acao")  
+print ("fime:", filme)
+print("genero:", genero)
+# tudo ok
+
+n1 = input ("informe a primeira palavra")
+n2 = input ("informe a segunda palavra")
+n3 = input ('informe a terceira palavra:')
+print("As palavras informadas foram:", n1,n2,n3)
+#tudo ok
+
+print("Camaragibe.")
+print("Pernambuco.")
+
+import datetime
+now = datetime.datetime.now()
+nome_usuario = input ('Digite o seu nome')
+print("olá", nome_usuario , "agora sao")
+print(now.strftime("%Y-%m-%d %H:%M:%S" ))
+# tudo ok
+
+nome_produto = input("Digite o nome do produto:")
+preco_produto = float(input('Digite o preco do produto:'))
+print ("O produto:", nome_produto)
+print ("custa R$", preco_produto)
+# tudo ok
+
+cidade = "Madrid"
+sigla_pais = "(ESP)"
+print("Eu gostaria de visitar", cidade,sigla_pais)
+# tudo ok
